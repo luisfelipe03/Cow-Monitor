69bff5a075d3bf381d86a3be6c66fba67740ae07
Luis Felipe <llipeandrade34@gmail.com>
2024-09-21 09:18:00 -0300

    Update Sensor.java

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,14 +1,10 @@
 public class Sensor {
     // Simulação de leitura do sensor via USB
-    // No projeto real, você usaria javax.usb para interagir com o hardware
+    // No projeto real, vamos usar a lib javax.usb para interagir com o hardware
 
     public String detectarTag() {
-        // Simulação de detecção de uma tag RFID (ID e ECC)
-        // Aqui você deve implementar a leitura real do sensor RFID via USB
+        // Simulação de detecção de uma tag RFID (ID)
+        // Aqui sera implementado a leitura real do sensor RFID via USB
         return "ID_VACA_001";  // Simulando o retorno de um ID
-    }
-
-    public String obterECC() {
-        return "ECC123";  // Simulando o retorno de um código ECC
     }
 }
